18abb6e1029ad506b222e37fb9c05c51ca1fbefa
Moosa-Hassan <alvimoosahassan@gmail.com>
2025-07-26 16:45:27 +0500

    Final Update

--- api/app.py
+++ api/app.py
@@ -4,6 +4,8 @@ from chatbot import (
     summarize_reviews, add_Instructor_review, load_course_reviews , add_Course_review
 )
 
+import os
+
 app = Flask(__name__, template_folder="templates")
 
 @app.route("/", methods=["GET", "POST"])
@@ -51,4 +53,5 @@ def handler(request, context):
     return app(request.environ, start_response=context["start_response"])
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+   port = int(os.environ.get("PORT", 5000))
+   app.run(host="0.0.0.0", port=port)
