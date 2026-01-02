d45ed31c8d371be75f0dde6e5b8941d7811ea437
Moosa Hassan Alvi <Alvimoosahassan@gmail.com>
2026-01-02 22:07:20 +0500

    syntax corrected

--- api/app.py
+++ api/app.py
@@ -52,5 +52,5 @@ def handler(request, context):
     return app(request.environ, start_response=context["start_response"])
 
 if __name__ == "__main__":
-   port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
